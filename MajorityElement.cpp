2fd8b90c54bce2f728c3c2a48611298df56466ad
Kshitij Pandey <60103525+kshitij4@users.noreply.github.com>
2021-02-14 18:08:08 +0530

    Add files via upload

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include<cmath>
+#include<algorithm>
 
 using namespace std;
 
-int major(int a[],int n)
+int majorNaive(int a[],int n)
 {
     int cnt,maxc=0;
     int idx=-1;;
@@ -26,11 +28,43 @@ int major(int a[],int n)
             return -1;
 }
 
+int majorUsingSort(int a[],int n)
+{
+    sort(a,a+n);
+    int count=1,prev = a[0],maxc=-1;
+    int element,flag=0;
+    for(int i=1;i<n;i++)
+    {
+        if(a[i] == prev)
+            count++;
+        else
+        {
+            count =1;
+            prev = a[i];
+        }
+        if(count > maxc)
+        {
+            maxc = count;
+            element = a[i];
+            if( maxc > n/2)
+            {
+                flag =1;
+                break;
+            }
+        }
+    }
+    if( flag == 0)
+        return -1;
+    else
+        return element;
+
+}
+
 int main()
 {
-    int arr[]={3, 3, 4, 2, 4, 4, 2, 4};
+    int arr[]={3, 3, 4, 2, 4, 4, 2, 4, 4};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int res = major(arr,n);
+    int res = majorUsingSort(arr,n);
     if(res == -1)
         cout<<"No Majority Element";
     else
