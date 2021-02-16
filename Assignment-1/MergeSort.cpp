bc8389e88819871c93a5e11498a5bd9c081fd970
Kshitij Pandey <60103525+kshitij4@users.noreply.github.com>
2021-02-16 16:56:00 +0530

    Add files via upload

--- Assignment-1/MergeSort.cpp
+++ Assignment-1/MergeSort.cpp
@@ -1,49 +1,67 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int partition (int a[], int low, int high)
+void merge(int arr[], int l, int m, int r)
 {
-    int pivot = a[high];
-    int i = (low - 1);
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (a[j] < pivot)
-        {
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-            swap(a[i],a[j]);
         }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    swap(a[i + 1], a[high]);
-    return (i + 1);
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void quickSort(int a[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(a, low, high);
-
-        quickSort(a, low, pi - 1);
-        quickSort(a, pi + 1, high);
+void mergeSort(int arr[],int l,int r){
+    if(l>=r){
+        return;
     }
+    int m =l+ (r-l)/2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
 }
-
-void printArray(int arr[], int size)
+void printArray(int a[], int n)
 {
-    for(int i = 0; i < size; i++)
-        cout << arr[i] << " ";
+
 }
 
 int main()
 {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {43,5,23,1,223,6,54,89,71};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    quickSort(arr, 0, n - 1);
+    mergeSort(arr, 0, n - 1);
 
-    printArray(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
