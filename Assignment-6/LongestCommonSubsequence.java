115f001f8f95aff66cdbe8bfeb8e556c365d3e10
Kshitij Pandey <60103525+kshitij4@users.noreply.github.com>
2021-02-22 21:53:44 +0530

    Add files via upload

---
+++ Assignment-6/LongestCommonSubsequence.java
@@ -0,0 +1,26 @@
+package DP;
+
+public class LongestCommonSubsequence {
+
+    public static int lcs(String text1, String text2)
+    {
+        int [][] dp = new int [text1.length()+1][text2.length()+1];
+        for(int i=1;i<dp.length;i++) {
+            for(int j=1;j<dp[0].length;j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "AGGTAB";
+        String s2 = "GXTXAYB";
+
+        System.out.println(lcs(s1,s2));
+
+    }
+}
